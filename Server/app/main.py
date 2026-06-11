464096efbaf8769073696be62da22eb14006ac2a
ishan-netizen <ishan@immersivequest.ai>
2026-06-11 16:58:17 +0530

    Fix CORS regex to match all Cloud Run URLs

--- Server/app/main.py
+++ Server/app/main.py
@@ -98,7 +98,7 @@ if settings.is_production():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origin_regex=r"https://.*\.a\.run\.app",
+    allow_origin_regex=r"https://.*\.run\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
